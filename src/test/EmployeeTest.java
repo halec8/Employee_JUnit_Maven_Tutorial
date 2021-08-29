fd1bf2862afba9cae381d2fd2e4b281136abea8f
Cody Hale <hale.l.c91@gmail.com>
2021-08-29 14:58:08 -0500

    first commit
    first commit

---
+++ src/test/EmployeeTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Cody Hale - chale
+ * CIS175 - Fall 2021
+ * Aug 29, 2021
+ */
+public class EmployeeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+		
+	}
+
+	@Test
+	public void testIsRetirementAge() {
+		
+	
+		
+	}
+
+}
